9643d70d890bb84f81d3a5d9a0410ca3c77a7696
Hadil kortas <kortashadil27@gmail.com>
2023-10-28 12:57:14 +0100

    update

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,12 +1,30 @@
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export const GET = () => {
-    return new NextResponse("Hello", { status: 200 });
-}
+
+const prisma = new PrismaClient()
+
+//FETCH ALL CATEGORIES
+export const GET = async () => {
+    try {
+        const categories = await prisma.category.findMany()
+        return new NextResponse(
+            JSON.stringify(categories),
+            { status: 200 }
+        );
+        
+    } catch (err) {
+        console.log(err);
+        return new NextResponse(
+            JSON.stringify({ message: "Something went wreong!" }),
+            { status: 500 }
+        );
+    }
+};
 
 export const POST = () => {
     return new NextResponse("Hello", { status: 201 });
 };
 
 
- 
+  
